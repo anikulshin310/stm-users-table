836f80dbd9cdc5f744f7fa2792c1f889757aa306
Andrey Nikulshin <anikulshin310@gmail.com>
2024-01-31 19:47:37 +0300

    add useDebounce and Input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,29 @@
+import React, { FC, useEffect, useState } from "react";
+import { useDebounce } from "../../hooks/useDebounce";
+
+interface IProps {
+  onChange: (value: string) => void;
+  onReset: () => void;
+}
+
+export const Input: FC<IProps> = (props) => {
+  const { onChange, onReset } = props;
+  const [value, setValue] = useState<string>("");
+  const debouncedValue = useDebounce(value, 1000);
+  const handleReset = () => {
+    setValue("");
+    onReset();
+  };
+  const handleValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+  useEffect(() => {
+    onChange(debouncedValue);
+  }, [debouncedValue]);
+  return (
+    <>
+      <input onChange={handleValueChange} value={value}></input>
+      <button onClick={handleReset}>Сбросить</button>
+    </>
+  );
+};
